9afc86bafee011226e7d6526e1d81f256617566a
MUH. RESTU ALIZA AKBAR <muhrestualizaakbar@gmail.com>
2024-08-19 22:19:50 +0800

    first

---
+++ app/dashboard/Chart.jsx
@@ -0,0 +1,114 @@
+"use client"
+
+import * as React from "react"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+    { status: "In Progress", visitors: 2, fill: "var(--color-progress)" },
+    { status: "New", visitors: 20, fill: "var(--color-new)" },
+    { status: "Cancelled", visitors: 23, fill: "var(--color-cancelled)" },
+    { status: "Done", visitors: 12, fill: "var(--color-done)" },
+]
+
+const chartConfig = {
+    visitors: {
+        label: "Visitors",
+    },
+    progress: {
+        label: "In Progress",
+        color: "#63b2fd",
+    },
+    new: {
+        label: "New",
+        color: "#9bdfc4",
+    },
+    cancelled: {
+        label: "Cancelled",
+        color: "#f99cab",
+    },
+    done: {
+        label: "Done",
+        color: "#ffb44f",
+    },
+}
+
+export function Chart() {
+    const totalVisitors = React.useMemo(() => {
+        return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
+    }, [])
+
+    return (
+        <Card className="flex flex-col">
+            <CardHeader className="items-center pb-0">
+                <CardTitle>Feedback Analytics</CardTitle>
+            </CardHeader>
+            <CardContent className="flex-1 pb-0">
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[250px]"
+                >
+                    <PieChart>
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Pie
+                            data={chartData}
+                            dataKey="visitors"
+                            nameKey="status"
+                            innerRadius={60}
+                            strokeWidth={5}
+                        >
+                            <Label
+                                content={({ viewBox }) => {
+                                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                        return (
+                                            <text
+                                                x={viewBox.cx}
+                                                y={viewBox.cy}
+                                                textAnchor="middle"
+                                                dominantBaseline="middle"
+                                            >
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={viewBox.cy}
+                                                    className="fill-foreground text-3xl font-bold"
+                                                >
+                                                    {totalVisitors.toLocaleString()}
+                                                </tspan>
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={(viewBox.cy || 0) + 24}
+                                                    className="fill-muted-foreground"
+                                                >
+                                                    Saran
+                                                </tspan>
+                                            </text>
+                                        )
+                                    }
+                                }}
+                            />
+                        </Pie>
+                    </PieChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Chart
